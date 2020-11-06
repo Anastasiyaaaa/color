59dfa23315348aecc28ffb4b956777b78657829b
Anastasiia Onyshchenko <a.onyshchenko@aimprosoft.com>
2020-11-06 16:32:36 +0200

    fixed v

--- src/DropArrowRGBItem.js
+++ src/DropArrowRGBItem.js
@@ -1,40 +1,16 @@
-import React, {useState, useContext}  from 'react'
-import Context from './context'
+import React, {useState}  from 'react'
 
 export default function DropArrowRGBItem(props){
-    const {tempColor} = useContext(Context);
-
     const [rangeVal, setRangeVal] = useState(props.value);
-    setRangeVal(tempColor.rgb)
-
-    const change = (v, i) => {
+    const change = (v) => {
         setRangeVal(+v);
-        props.onChange(+v, i);
+        props.onChange(+v);
     };
 
     return(
         <li>
-            <span>{props.letter}</span>
-            <input type="range" onChange={(e) => change(e.target.value, props.index)} value={rangeVal} min="0" max="255" className={`range range${props.index}`} />
+            <span>{props.text}</span>
+            <input type="range" onChange={(e) => change(e.target.value)} value={rangeVal} min="0" max="255"  className={`range range${props.i}`} />
         </li>
     )
 }
-
-// export default function DropArrowRGBItem(props){
-//
-//     const [rangeVal, setRangeVal] = useState(props.value);
-//
-//
-//     const change = (v) => {
-//         setRangeVal(+v);
-//         props.onChange(+v);
-//     };
-//
-//
-//     return(
-//         <li>
-//         <span>{props.text}</span>
-//         <input type="range" onChange={(e) => change(e.target.value)} value={rangeVal} min="0" max="255"  className={`range range${props.i}`} />
-//         </li>
-//     )
-// }
--- src/DropDownsRGB.js
+++ src/DropDownsRGB.js
@@ -1,4 +1,4 @@
-import React, {useContext, useRef, useState} from 'react'
+import React, {useContext, useRef} from 'react'
 import Context from './context'
 import DropArrowRGBItem from './DropArrowRGBItem'
 import Buttons from './Buttons'
@@ -7,46 +7,27 @@ export default function DropDowns() {
     const {currentColor, setTempColor, tempColor, setShowRGBRange, showRGBRange, useOutsideAlerter} = useContext(Context);
     const closeDropDown = useRef(null);
     useOutsideAlerter(closeDropDown,  setShowRGBRange, showRGBRange);
-    const [letterRGB, setLetterRGB] = useState('B');
-
-
 
     if (!tempColor ) {
         setTempColor(currentColor);
     }
-    const change = (v, i) => {
-        switch (i) {
-            case 0:
-                setLetterRGB("R");
-                setTempColor({ ...tempColor, rgb: [v, tempColor.rgb[1], tempColor.rgb[2]]});
-                break;
-            case 1:
-                setLetterRGB("G");
-                setTempColor({...tempColor, rgb: [tempColor.rgb[0], v, tempColor.rgb[2]]});
-                break;
-            default:
-                setTempColor({ ...tempColor, rgb: [tempColor.rgb[0], tempColor.rgb[1], v]});
-            }
+    const changeR = (v) => {
+        setTempColor({ ...tempColor, rgb: [v, tempColor.rgb[1], tempColor.rgb[2]]});
     };
-    // const changeR = (v) => {
-    //     setTempColor({ ...tempColor, rgb: [v, tempColor.rgb[1], tempColor.rgb[2]]});
-    // };
-    // const changeG = (v) => {
-    //     setTempColor({...tempColor, rgb: [tempColor.rgb[0], v, tempColor.rgb[2]]});
-    // };
-    // const changeB = (v) => {
-    //     setTempColor({ ...tempColor, rgb: [tempColor.rgb[0], tempColor.rgb[1], v]});
-    // };
-    {/*<DropArrowRGBItem value={tempColor.rgb[0]} onChange={changeR} text='R' i='1'/>*/}
-    {/*<DropArrowRGBItem value={tempColor.rgb[1]}   onChange={changeG} text='G' i='2'/>*/}
-    {/*<DropArrowRGBItem value={tempColor.rgb[2]}   onChange={changeB} text='B' i='3'/>*/}
+    const changeG = (v) => {
+        setTempColor({...tempColor, rgb: [tempColor.rgb[0], v, tempColor.rgb[2]]});
+    };
+    const changeB = (v) => {
+        setTempColor({ ...tempColor, rgb: [tempColor.rgb[0], tempColor.rgb[1], v]});
+    };
 
     return (
         tempColor &&
         <div className='dropdown dropdown_rgb' ref={closeDropDown}>
             <ul>
-                {tempColor.rgb.map((value, index) => <DropArrowRGBItem key={index} letter={letterRGB} value={value} onChange={change} index={index} />)}
-
+                <DropArrowRGBItem value={tempColor.rgb[0]} onChange={changeR} text='R' i='1'/>
+                <DropArrowRGBItem value={tempColor.rgb[1]}   onChange={changeG} text='G' i='2'/>
+                <DropArrowRGBItem value={tempColor.rgb[2]}   onChange={changeB} text='B' i='3'/>
             </ul>
             <Buttons />
         </div>
